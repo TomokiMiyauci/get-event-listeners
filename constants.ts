66376c3835800ebf786a0c450da4a3ed36ab50ee
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-10 02:05:34 +0900

    refactor: remove unused

--- constants.ts
+++
@@ -1,9 +0,0 @@
-// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
-// This module is browser compatible.
-
-import type { EventListenerMap } from "./map.ts";
-
-export const eventTargetRegistry = /*@__PURE__*/ new WeakMap<
-  object,
-  EventListenerMap
->();
